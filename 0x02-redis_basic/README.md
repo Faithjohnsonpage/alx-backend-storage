31816822c8485a125f533473c69a9aec7c5c65da
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-07-17 21:40:10 +0100

    exercise.py

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,3 @@
+# Redis basic
+
+These are solutions to tasks on Redis basic
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This module implements a redis task"""
+import redis
+import uuid
+from typing import TypeVar
+
+
+T = TypeVar('T')
+
+
+class Cache:
+    def __init__(self):
+        """
+        Connects to the Redis server and initializes the cache object.
+        """
+        self._redis = redis.Redis()
+
+    def store(self, data: T) -> str:
+        """
+        Stores the provided data in the cache and returns a unique key.
+
+        Args:
+            data: The data to be stored (can be any type).
+
+        Returns:
+            A string representing the unique key assigned to the data.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
